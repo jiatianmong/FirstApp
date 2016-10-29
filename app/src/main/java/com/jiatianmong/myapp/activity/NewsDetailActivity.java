adb760a35876d883d8856009e1ed7a94a7e9e853
jiatianmong <512184992@qq.com>
2016-10-29 16:26:53 +0800

    设置返回，调整字体大小，分享的点击事件。通过sharesdk分享。加载网页新闻是加上一个类似微信的进度条

--- app/src/main/java/com/jiatianmong/myapp/activity/NewsDetailActivity.java
+++ app/src/main/java/com/jiatianmong/myapp/activity/NewsDetailActivity.java
@@ -17,6 +17,11 @@ import android.widget.ProgressBar;
 import com.jiatianmong.myapp.R;
 import com.lidroid.xutils.ViewUtils;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+
+
 /**
  * 新闻详情页面
  *
@@ -26,147 +31,179 @@ import com.lidroid.xutils.ViewUtils;
 public class NewsDetailActivity extends Activity implements OnClickListener {
 
 
-	private WebView mWebView;
-	private ProgressBar mProgressBar;
-	private String mUrl;
-	private ImageButton btBack;
-	private ImageButton btSize;
-	private ImageButton btShare;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+    private String mUrl;
+    private ImageButton btBack;
+    private ImageButton btSize;
+    private ImageButton btShare;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_news_detail);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_news_detail);
 
-		ViewUtils.inject(this);
-		mProgressBar = (ProgressBar) findViewById(R.id.pb_Newsdetail);
-		btBack = (ImageButton) findViewById(R.id.btn_back);
-		btSize = (ImageButton) findViewById(R.id.btn_size);
-		btShare = (ImageButton) findViewById(R.id.btn_share);
-		btBack.setOnClickListener(this);
-		btSize.setOnClickListener(this);
-		btShare.setOnClickListener(this);
+        ViewUtils.inject(this);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_Newsdetail);
+        btBack = (ImageButton) findViewById(R.id.btn_back);
+        btSize = (ImageButton) findViewById(R.id.btn_size);
+        btShare = (ImageButton) findViewById(R.id.btn_share);
+        btBack.setOnClickListener(this);
+        btSize.setOnClickListener(this);
+        btShare.setOnClickListener(this);
 
-		mWebView = (WebView) findViewById(R.id.wv_content);
-		WebSettings settings = mWebView.getSettings();
-		settings.setBuiltInZoomControls(true);// 显示缩放按钮(wap网页不支持)
-		settings.setUseWideViewPort(true);// 支持双击缩放(wap网页不支持)
-		settings.setJavaScriptEnabled(true);// 支持js功能
+        mWebView = (WebView) findViewById(R.id.wv_content);
+        WebSettings settings = mWebView.getSettings();
+        settings.setBuiltInZoomControls(true);// 显示缩放按钮(wap网页不支持)
+        settings.setUseWideViewPort(true);// 支持双击缩放(wap网页不支持)
+        settings.setJavaScriptEnabled(true);// 支持js功能
 
-		mUrl = getIntent().getStringExtra("url");
-		mWebView.loadUrl(mUrl);
-
-
-		mWebView.setWebViewClient(new WebViewClient() {
-			// 开始加载网页
-			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				super.onPageStarted(view, url, favicon);
-				System.out.println("开始加载网页了");
-				mProgressBar.setVisibility(View.VISIBLE);
-			}
-
-			// 网页加载结束
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-				System.out.println("网页加载结束");
-				mProgressBar.setVisibility(View.INVISIBLE);
-			}
-
-			// 所有链接跳转会走此方法
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				System.out.println("跳转链接:" + url);
-				view.loadUrl(url);// 在跳转链接时强制在当前webview中加载
-				return true;
-			}
-		});
-
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-			case R.id.btn_back:
-			//
-				finish();
-			break;
-			case R.id.btn_size:
-				// 修改网页字体大小
-				showChooseDialog();
-			break;
-			case R.id.btn_share:
-			//
-
-			break;
-		}
-	}
+        mUrl = getIntent().getStringExtra("url");
+        mWebView.loadUrl(mUrl);
 
 
-	private int mTempWhich;// 记录临时选择的字体大小(点击确定之前)
+        mWebView.setWebViewClient(new WebViewClient() {
+            // 开始加载网页
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                System.out.println("开始加载网页了");
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
 
-	private int mCurrenWhich = 2;// 记录当前选中的字体大小(点击确定之后), 默认正常字体
+            // 网页加载结束
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                System.out.println("网页加载结束");
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
 
-	private void showChooseDialog() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("字体设置");
+            // 所有链接跳转会走此方法
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                System.out.println("跳转链接:" + url);
+                view.loadUrl(url);// 在跳转链接时强制在当前webview中加载
+                return true;
+            }
+        });
 
-		String[] items = new String[] { "超大号字体", "大号字体", "正常字体", "小号字体",
-				"超小号字体" };
+    }
 
-		builder.setSingleChoiceItems(items, mCurrenWhich,
-				new DialogInterface.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_back:
+                //
+                finish();
+                break;
+            case R.id.btn_size:
+                // 修改网页字体大小
+                showChooseDialog();
+                break;
+            case R.id.btn_share:
+                //
+                showShare();
+                break;
+        }
+    }
 
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						mTempWhich = which;
-					}
-				});
 
-		builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
 
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// 根据选择的字体来修改网页字体大小
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
 
-				WebSettings settings = mWebView.getSettings();
+        // 启动分享GUI
+        oks.show(this);
+    }
 
-				switch (mTempWhich) {
-					case 0:
-						// 超大字体
-						settings.setTextSize(WebSettings.TextSize.LARGEST);
-						// settings.setTextZoom(22);
-						break;
-					case 1:
-						// 大字体
-						settings.setTextSize(WebSettings.TextSize.LARGER);
-						break;
-					case 2:
-						// 正常字体
-						settings.setTextSize(WebSettings.TextSize.NORMAL);
-						break;
-					case 3:
-						// 小字体
-						settings.setTextSize(WebSettings.TextSize.SMALLER);
-						break;
-					case 4:
-						// 超小字体
-						settings.setTextSize(WebSettings.TextSize.SMALLEST);
-						break;
 
-					default:
-						break;
-				}
+    private int mTempWhich;// 记录临时选择的字体大小(点击确定之前)
 
-				mCurrenWhich = mTempWhich;
-			}
-		});
+    private int mCurrenWhich = 2;// 记录当前选中的字体大小(点击确定之后), 默认正常字体
 
-		builder.setNegativeButton("取消", null);
+    private void showChooseDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("字体设置");
 
-		builder.show();
-	}
+        String[] items = new String[]{"超大号字体", "大号字体", "正常字体", "小号字体",
+                "超小号字体"};
+
+        builder.setSingleChoiceItems(items, mCurrenWhich,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mTempWhich = which;
+                    }
+                });
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 根据选择的字体来修改网页字体大小
+
+                WebSettings settings = mWebView.getSettings();
+
+                switch (mTempWhich) {
+                    case 0:
+                        // 超大字体
+                        settings.setTextSize(WebSettings.TextSize.LARGEST);
+                        // settings.setTextZoom(22);
+                        break;
+                    case 1:
+                        // 大字体
+                        settings.setTextSize(WebSettings.TextSize.LARGER);
+                        break;
+                    case 2:
+                        // 正常字体
+                        settings.setTextSize(WebSettings.TextSize.NORMAL);
+                        break;
+                    case 3:
+                        // 小字体
+                        settings.setTextSize(WebSettings.TextSize.SMALLER);
+                        break;
+                    case 4:
+                        // 超小字体
+                        settings.setTextSize(WebSettings.TextSize.SMALLEST);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                mCurrenWhich = mTempWhich;
+            }
+        });
+
+        builder.setNegativeButton("取消", null);
+
+        builder.show();
+    }
 }
